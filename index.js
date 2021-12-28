7a84586b09323f34856fc5b2a32f9f9f6ffb08ba
sarim-dev <sarimhassan5@gmail.com>
2021-12-29 00:39:01 +0500

    API

---
+++ index.js
@@ -0,0 +1,45 @@
+const PORT=process.env.PORT || 8000;
+const axios=require("axios");
+const cheerio=require("cheerio");
+const { contains } = require("cheerio/lib/static");
+const express = require("express");
+const app=express();
+const companiesname=[];
+const DTSnumber=[];
+const email=[];
+const verifiedcomp=[]
+app.get("/",(req,res)=>{
+    res.send("API DEVELOPMENT");
+})
+
+
+app.get("/verifiedcompanies",async(req,res)=>{
+  await  axios.get("https://visa.nadra.gov.pk/list-of-tour-operators/").then((res)=>{ 
+     const html=res.data;
+   const $=cheerio.load(html);
+
+   //get all companies name
+$('td[data-x="1"]',html).each(function(){companiesname.push($(this).text());})
+;
+
+//get all dts numbers
+$('td[data-x="2"]',html).each(function(){ DTSnumber.push($(this).text());})
+
+//get all companies email
+// $('td[data-x="9"]',html).each(function(){ email.push($(this).text());})
+   
+
+}).catch(error=>{console.log(error)})
+
+ for(let i=0;i<DTSnumber.length;i++)
+ {
+      verifiedcomp.push({
+          No:i+1,
+          CompanyName:companiesname[i],
+          DtsNumber:DTSnumber[i]
+      })
+ }
+   res.send(verifiedcomp)
+})
+
+app.listen(PORT,()=>{console.log(`SERVER IS STARTED ON ${PORT}`)});
