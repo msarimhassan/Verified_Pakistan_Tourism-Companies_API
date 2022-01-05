e66a39c4ae432714cf0feccb52c3795e0e2621a1
sarim-dev <sarimhassan5@gmail.com>
2022-01-05 21:01:34 +0500

    Added new function

--- index.js
+++ index.js
@@ -46,4 +46,54 @@ $('td[data-x="2"]',html).each(function(){ DTSnumber.push($(this).text());})
    res.send(verifiedcomp)
 })
 
+//get company by parameter
+
+app.get('/getcompany/:name/:dts',async(req,res)=>{
+  console.log(req.params.name)
+  console.log(req.params.dts)
+  const companiesname=[];
+const DTSnumber=[];
+const email=[];
+const verifiedcomp=[]
+  await  axios.get("https://visa.nadra.gov.pk/list-of-tour-operators/").then((res)=>{ 
+     const html=res.data;
+   const $=cheerio.load(html);
+
+   //get all companies name
+$('td[data-x="1"]',html).each(function(){companiesname.push($(this).text());})
+;
+
+//get all dts numbers
+$('td[data-x="2"]',html).each(function(){ DTSnumber.push($(this).text());})
+
+//get all companies email
+// $('td[data-x="9"]',html).each(function(){ email.push($(this).text());})
+   
+
+}).catch(error=>{console.log(error)})
+
+ for(let i=0;i<DTSnumber.length;i++)
+ {
+      verifiedcomp.push({
+          No:i+1,
+          CompanyName:companiesname[i],
+          DtsNumber:DTSnumber[i]
+      })
+ }
+    //Find desired Objects
+      
+    let companyName=req.params.name;
+    let dtsNumber=req.params.dts;
+
+verifiedcomp.forEach(company=>{
+        if(company.DtsNumber.trim().toLowerCase()==dtsNumber.toLowerCase() && company.CompanyName.trim().toLowerCase()==companyName.toLowerCase())
+        {
+          return res.send(true);
+        }
+    }) 
+   
+res.send(false);
+
+})
+
 app.listen(PORT,()=>{console.log(`SERVER IS STARTED ON ${PORT}`)});
